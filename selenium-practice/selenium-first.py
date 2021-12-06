4c36f642bacf0bfe1b158e61a39d6cec22b4086a
Abdulrazzak Jaroukh <abdelrazaq.ja@gmail.com>
2021-12-06 17:27:23 +0300

    practice session 1

--- selenium-practice/selenium-first.py
+++ selenium-practice/selenium-first.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-# import os
 
-# os.environ['PATH'] += r"C:\SeleniumDrivers\chromedriver.exe"
 
 s = Service(r"C:\Users\Aboudi\PycharmProjects\SeleniumDrivers\chromedriver.exe")
-
 driver = webdriver.Chrome(service=s)
-
 driver.get("http://www.seleniumframework.com/Practiceform/")
-
-driver.implicitly_wait(30)
-
+# driver.implicitly_wait(30)
 my_button = driver.find_element(By.ID, "button1")
-my_button.click()
+print(my_button.text)  # can be commented
+# my_button.click()
+
+WebDriverWait(driver, 42).until(EC.text_to_be_present_in_element(
+    (By.ID, 'clock'), 'Buzz Buzz'))
